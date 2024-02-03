b1defa36005713db8423b5bd0ddbd88664a48d5f
Arfan Abid <iffionex111@gmail.com>
2024-02-03 23:50:33 +0500

    ~ detail GET,PUT,DELETE  Configered also API is tested

--- Backend/core/Blog/urls.py
+++ Backend/core/Blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.blog_list),
+    path('<int:id>', views.blog_detail),
     
 ]
--- Backend/core/Blog/views.py
+++ Backend/core/Blog/views.py
@@ -20,10 +20,35 @@ def blog_list(request, *args, **kwargs):
     if request.method== 'GET':
         queryset=Blog.objects.all()
         serializer=BlogSerializer(queryset, many=True)
-        return JsonResponse({'Blog':serializer.data})     #  OR   return JsonResponse(serializer.data,safe=False)
+        return Response({'Blog':serializer.data})     #  OR   return JsonResponse(serializer.data,safe=False)
     
     if request.method == 'POST':
         serializer=BlogSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
+
+
+
+@api_view(['GET','PUT','DELETE'])
+def blog_detail(request ,id):
+    try:
+        query=Blog.objects.get(pk=id)
+    except Blog.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)    
+
+
+    if request.method=='GET':
+        serializer=BlogSerializer(query)
+        return Response(data=serializer.data)
+    
+    if request.method=='PUT':
+        serializer=BlogSerializer(query,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method=='DELETE':
+        query.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
