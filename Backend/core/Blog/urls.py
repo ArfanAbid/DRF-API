f055219b6e4cc244baaae4d1db2765f4b265de95
Arfan Abid <iffionex111@gmail.com>
2024-02-08 13:15:33 +0500

    ~ Configered Class Based Views

--- Backend/core/Blog/urls.py
+++ Backend/core/Blog/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blog_list),
-    path('<int:id>', views.blog_detail),
+    path('', views.blog_list), # Function based
+    path('<int:id>', views.blog_detail), # Function based
+    path('list/', views.ListBlog.as_view()), # Class based
+    path('detail/<int:pk>/', views.DetailBlog.as_view()), # Class based
     
 ]
--- Backend/core/Blog/views.py
+++ Backend/core/Blog/views.py
@@ -7,11 +7,15 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 
 
 from .models import Blog
 from .serializers import BlogSerializer
 
+# There are many ways to write views in DRF which are listed below :
+
+    # Function based views
 
 @api_view(['GET','POST'])
 def blog_list(request, *args, **kwargs):
@@ -52,3 +56,54 @@ def blog_detail(request ,id):
     if request.method=='DELETE':
         query.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+    # Class based Views
+
+
+class ListBlog(APIView):
+    
+    def get(self,request,*args,**kwargs):
+        queryset=Blog.objects.all()
+        serializer=BlogSerializer(queryset,many=True)
+        return Response({'Blog':serializer.data})
+    
+    def post(self,request,*args,**kwargs):
+        serializer=BlogSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'Blog':serializer.data},status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+class DetailBlog(APIView):
+
+    def get(self, request,pk,*args, **kwargs):
+        try:
+            query=Blog.objects.get(pk=pk)
+        except Blog.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        serializer=BlogSerializer(query)
+        return Response(serializer.data)
+
+    def put(self,request,pk,*args, **kwargs):
+        try:
+            query=Blog.objects.get(pk=pk)
+        except Blog.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        serializer=BlogSerializer(query,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self,request,pk,*args, **kwargs):
+        try:
+            query=Blog.objects.get(pk=pk)
+        except Blog.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)     
+
+        query.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)       
+    
