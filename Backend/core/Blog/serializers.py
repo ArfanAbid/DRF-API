2c0b652524114b5b90cada084daec9a8811f6b71
Arfan Abid <iffionex111@gmail.com>
2024-02-08 15:17:37 +0500

    ~ Configered  Validation in Serializers

--- Backend/core/Blog/serializers.py
+++ Backend/core/Blog/serializers.py
@@ -5,3 +5,12 @@ class BlogSerializer(serializers.ModelSerializer):
     class Meta:
         model=Blog
         fields=('id','title','content')
+        
+        #fields=__all__
+        #exclude=('id)
+
+        def validate_data(self, attrs):
+            title =attrs.get('title')
+            if Blog.objects.filter(title=title).exists(): 
+                raise serializers.ValidationError("A blog with this title already exists.")
+            return attrs
